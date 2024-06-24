7296fa919295acb03e7d09220b4c233c00469fef
theeasylearn <theeasylearn@gmail.com>
2024-06-24 18:28:46 +0530

    stack

--- firstapp/lib/main.dart
+++ firstapp/lib/main.dart
@@ -1,41 +1,57 @@
 import 'package:flutter/material.dart';
 void main() {
-  StackDemo1 s1 = new StackDemo1();
-  runApp(s1);
+  ProductDetail pd1 = new ProductDetail();
+  runApp(pd1);
 }
-class MyContainer extends StatelessWidget {
-  double height=0,width=0;
-  Color color = Colors.black;
-  MyContainer(double height, double width, Color color)
-  {
-      this.height = height;
-      this.width = width;
-      this.color = color;
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        height: this.height,
-        width: this.width,
-        color: this.color,
-    );
-  }
-}
-class StackDemo1 extends StatelessWidget {
-  const StackDemo1({super.key});
+//test
+class ProductDetail extends StatelessWidget {
+  const ProductDetail({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Center(
+      home: Material(
+          color: Colors.white30,
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: double.infinity,
             child: Stack(
-                alignment: Alignment.center,
-                children: [
-                  MyContainer(200,200,Colors.red),
-                  MyContainer(150,150,Colors.green),
-                  MyContainer(100,100,Colors.yellow),
-                ],
+              alignment: Alignment.topLeft,
+              children: [
+                  Positioned(
+                      left: 0,
+                      top: 0,
+                      child: Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: 250,
+                          color: Colors.grey,
+                          alignment: Alignment.center,
+                          child: FlutterLogo(
+                              size: 250,
+                          ),
+                      )
+                  ),
+                  Positioned(
+                      left: 0,
+                      bottom: 0,
+                      child: Container(
+                          height: 50,
+                          width: MediaQuery.of(context).size.width,
+                          color: Colors.redAccent,
+                          alignment: Alignment.center,
+                          child: Text(
+                              "Add to cart",
+                            textDirection: TextDirection.ltr,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24
+                            ),
+                          ),
+                      )
+                  ),
+              ],
             ),
-        ),
+          ),
+      ),
     );
   }
 }
