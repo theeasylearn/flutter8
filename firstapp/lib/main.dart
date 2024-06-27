a06c9efc19def993ef1b1634f1b149fb2fb92cd2
theeasylearn <theeasylearn@gmail.com>
2024-06-27 20:05:48 +0530

    AspectRatio

--- firstapp/lib/main.dart
+++ firstapp/lib/main.dart
@@ -1,59 +1,41 @@
 import 'package:flutter/material.dart';
 void main() {
-  SizedBoxDemo s1 = new SizedBoxDemo();
+  AspectRatio1 s1 = new AspectRatio1();
   runApp(s1);
 }
-class SizedBoxDemo extends StatelessWidget {
+class AspectRatio1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Holiday Package",
-      home: Scaffold(
-        appBar: AppBar(title: Text("Holiday Package"),),
-        body: Material(
-          child: SizedBox(
-              height: 200,
-              width: double.infinity,
-              child: Card(
-                elevation: 6.0,
-                color: Colors.white24,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                        padding: EdgeInsets.all(10),
-                        color: Colors.deepPurple,
-                        child: Text("Himaliyan Heights (6D/5N)", style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold
-                        ),),
+        title: "Aspect Ratio example",
+        home: Scaffold(
+            appBar: AppBar(title: Text("Aspect ratio"),),
+            body: Material(
+                child: SizedBox(
+                    width:MediaQuery.of(context).size.width,
+                    height:MediaQuery.of(context).size.height/1.6,
+                    child: Card(
+                        elevation: 8,
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              AspectRatio(
+                                  aspectRatio: 16/9,
+                                  child: Ink.image(
+                                      image: NetworkImage('https://picsum.photos/400/200'),
+                                      fit: BoxFit.cover,
+                                      ),
+                              ),
+                              Container(
+                                  padding: EdgeInsets.all(5),
+                                  child: Text("India Vs England"),)
+                            ],
+                        ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 10),
-                      child: Text("Rs 55000"),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 10),
-                      child: Text("Air Tickets, 5star Hotels, Meals"),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 10),
-                      child: Text("5 Activity, Veshnavdevi temple"),
-                    ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width,
-                      child: ElevatedButton(onPressed: (){
-
-                      }, child: Text("Book now")),
-                    )
-                  ],
                 ),
-              ),
-          ),
+            ),
         ),
-      ),
     );
   }
 }
