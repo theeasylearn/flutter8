cb3450fc692ce51132ed1c290c320890908031c2
theeasylearn <theeasylearn@gmail.com>
2024-08-23 20:17:09 +0530

    customlistview

--- app3/lib/main.dart
+++ app3/lib/main.dart
@@ -4,6 +4,7 @@ import 'app_splash.dart';
 import 'app_view_expense.dart';
 import 'simple_interest_calc.dart';
 import 'package:intl/intl.dart';
+
 //https://github.com/nbnD/date_picker/blob/master/lib/homepage.dart
 void main() {
   runApp(AddTransaction());
@@ -17,6 +18,8 @@ class AddTransaction extends StatefulWidget {
 
 class _AddTransactionState extends State<AddTransaction> {
   TextEditingController dateController = new TextEditingController();
+
+  String SelectedValue = '1';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,86 +34,149 @@ class _AddTransactionState extends State<AddTransaction> {
                 padding: EdgeInsets.all(10),
                 child: LayoutBuilder(builder: (ctx,parent){
                     return SizedBox(
-                        height: parent.maxHeight * 0.80,
+                        height: parent.maxHeight,
                         child: SingleChildScrollView(
                             child: Card(
                                 elevation: 10,
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    TextField(
-                                      keyboardType: TextInputType.text,
-                                      decoration: InputDecoration(
-                                        fillColor: new Color(0x75ffffff),
-                                        filled:
-                                        true, // Ensures the background color is applied
-                                        enabledBorder: OutlineInputBorder(
-                                          borderSide: BorderSide(
-                                            color: Colors.black, // Black border color
-                                            width: 1.0, // Border width
-                                          ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(10),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      FittedBox(
+                                        child: Text(
+                                          "Add Entry",
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 24,
+                                              fontFamily: "sfpro"),
                                         ),
-                                        focusedBorder: OutlineInputBorder(
-                                          borderSide: BorderSide(
-                                            color: Colors
-                                                .black, // Black border color when focused
-                                            width: 1.0, // Border width when focused
+                                      ),
+                                      TextField(
+                                        keyboardType: TextInputType.text,
+                                        decoration: InputDecoration(
+                                          fillColor: new Color(0x75ffffff),
+                                          filled:
+                                          true, // Ensures the background color is applied
+                                          enabledBorder: OutlineInputBorder(
+                                            borderSide: BorderSide(
+                                              color: Colors.black, // Black border color
+                                              width: 1.0, // Border width
+                                            ),
+                                          ),
+                                          focusedBorder: OutlineInputBorder(
+                                            borderSide: BorderSide(
+                                              color: Colors
+                                                  .black, // Black border color when focused
+                                              width: 1.0, // Border width when focused
+                                            ),
                                           ),
                                         ),
                                       ),
-                                    ),
-                                    TextField(
-                                      keyboardType: TextInputType.number,
-                                      decoration: InputDecoration(
-                                        fillColor: new Color(0x75ffffff),
-                                        filled:
-                                        true, // Ensures the background color is applied
-                                        enabledBorder: OutlineInputBorder(
-                                          borderSide: BorderSide(
-                                            color: Colors.black, // Black border color
-                                            width: 1.0, // Border width
+                                      TextField(
+                                        keyboardType: TextInputType.number,
+                                        decoration: InputDecoration(
+                                          fillColor: new Color(0x75ffffff),
+                                          filled:
+                                          true, // Ensures the background color is applied
+                                          enabledBorder: OutlineInputBorder(
+                                            borderSide: BorderSide(
+                                              color: Colors.black, // Black border color
+                                              width: 1.0, // Border width
+                                            ),
                                           ),
-                                        ),
-                                        focusedBorder: OutlineInputBorder(
-                                          borderSide: BorderSide(
-                                            color: Colors
-                                                .black, // Black border color when focused
-                                            width: 1.0, // Border width when focused
+                                          focusedBorder: OutlineInputBorder(
+                                            borderSide: BorderSide(
+                                              color: Colors
+                                                  .black, // Black border color when focused
+                                              width: 1.0, // Border width when focused
+                                            ),
                                           ),
                                         ),
                                       ),
-                                    ),
-                                    TextField(
-                                      controller: dateController, //editing controller of this TextField
+                                      TextField(
+                                        controller: dateController, //editing controller of this TextField
                                         decoration: const InputDecoration(
                                             icon: Icon(Icons.calendar_today), //icon of text field
                                             labelText: "Enter Date" //label text of field
                                         ),
-                                        readOnly: true,  // when true user cannot edit text
-                                      onTap: () async {
-                                        DateTime? pickedDate = await showDatePicker(
-                                            context: context,
-                                            initialDate: DateTime.now(), //get today's date
-                                            firstDate: DateTime(2000), //DateTime.now() - not to allow to choose before today.
-                                            lastDate: DateTime(2101)
-                                        );
+                                        onTap: () async {
+                                          DateTime? pickedDate = await showDatePicker(
+                                              context: context,
+                                              initialDate: DateTime.now(), //get today's date
+                                              firstDate: DateTime(2000), //DateTime.now() - not to allow to choose before today.
+                                              lastDate: DateTime(2101)
+                                          );
 
-                                        if(pickedDate != null ){
-                                          print(pickedDate);  //get the picked date in the format => 2022-07-04 00:00:00.000
-                                          String formattedDate = DateFormat('yyyy-MM-dd').format(pickedDate); // format date in required form here we use yyyy-MM-dd that means time is removed
-                                          print(formattedDate); //formatted date output using intl package =>  2022-07-04
-                                          //You can format date as per your need
+                                          if (pickedDate != null) {
+                                            String formattedDate = DateFormat('dd-MM-yyyy').format(pickedDate);
+                                            setState(() {
+                                              dateController.text = formattedDate; //set formatted date to TextField value.
+                                            });
+                                          } else {
+                                            print("Date is not selected");
+                                          }
+                                        },
+                                      ),
+                                      TextField(
+                                        keyboardType: TextInputType.text,
+                                        decoration: InputDecoration(
+                                          fillColor: new Color(0x75ffffff),
+                                          filled:
+                                          true, // Ensures the background color is applied
+                                          enabledBorder: OutlineInputBorder(
+                                            borderSide: BorderSide(
+                                              color: Colors.black, // Black border color
+                                              width: 1.0, // Border width
+                                            ),
+                                          ),
+                                          focusedBorder: OutlineInputBorder(
+                                            borderSide: BorderSide(
+                                              color: Colors
+                                                  .black, // Black border color when focused
+                                              width: 1.0, // Border width when focused
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                      Text("Mode of entry"),
+                                      ListTile(
+                                        title: Text('Income'),
+                                        leading: Radio(
+                                          value: '2',
+                                          groupValue: SelectedValue,
+                                          onChanged: (value) {
+                                            setState(() {
+                                              SelectedValue = value.toString();
+                                            });
+                                          },
+                                        ),
+                                      ),
+                                      ListTile(
+                                        title: Text('Expense'),
+                                        leading: Radio(
+                                          value: '1',
+                                          groupValue: SelectedValue,
+                                          onChanged: (value) {
+                                            setState(() {
+                                              SelectedValue = value.toString();
+                                            });
+                                          },
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        width: parent.maxWidth,
+                                        child: ElevatedButton(
+                                            onPressed: () {
+                                              setState(() {
 
-                                          setState(() {
-                                            dateController.text = formattedDate; //set foratted date to TextField value.
-                                          });
-                                        }else{
-                                          print("Date is not selected");
-                                        }
-                                      },
-                                    )
-                                  ],
+                                              });
+                                            },
+                                            child: Text("Add Entry")),
+                                      ),
+                                    ],
+                                  ),
                                 ),
                             ),
                         ),
