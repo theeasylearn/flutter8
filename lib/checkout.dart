74418d5471f2a3c73f651c3b598ebc5d2dddfa58
theeasylearn <theeasylearn@gmail.com>
2024-11-28 20:16:08 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/cart.dart

--- lib/checkout.dart
+++ lib/checkout.dart
@@ -6,9 +6,10 @@ class Checkout extends StatefulWidget {
   @override
   State<Checkout> createState() => _CheckoutState();
 }
-
-class _CheckoutState extends State<Checkout> {
-    @override
+enum PaymentMethod { none,cashOnDelivery, online }
+  class _CheckoutState extends State<Checkout> {
+  PaymentMethod selectedPaymentMethod = PaymentMethod.cashOnDelivery;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: MyNavigationBar.getNavigationBar(),
@@ -17,12 +18,13 @@ class _CheckoutState extends State<Checkout> {
         child: Padding(
           padding: const EdgeInsets.only(top:50,left: 10,right: 10),
           child: SingleChildScrollView(
-               : Card(
+                child: Card(
                 elevation: 10,
                 child: Padding(
                     padding: EdgeInsets.all(10),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                           TextField(
                               keyboardType: TextInputType.text,
@@ -41,7 +43,7 @@ class _CheckoutState extends State<Checkout> {
                             height: 10,
                           ),
                           TextField(
-                              keyboardType: TextInputType.text,
+                              keyboardType: TextInputType.phone,
                               decoration: InputDecoration(
                                     prefixIcon: Icon(Icons.phone),
                                     labelText: "mobile",
@@ -53,6 +55,128 @@ class _CheckoutState extends State<Checkout> {
                                     )
                               ),
                           ),
+                          SizedBox(
+                          height: 10,
+                          ),
+                        TextField(
+                          keyboardType: TextInputType.text,
+                          decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.pin_drop),
+                              labelText: "Address Line 1",
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: AppColors.accentColor(),
+                                      strokeAlign:BorderSide.strokeAlignOutside
+                                  )
+                              )
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        TextField(
+                          keyboardType: TextInputType.text,
+                          decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.pin_drop),
+                              labelText: "Address Line 2",
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: AppColors.accentColor(),
+                                      strokeAlign:BorderSide.strokeAlignOutside
+                                  )
+                              )
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        TextField(
+                          keyboardType: TextInputType.text,
+                          decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.pin_drop),
+                              labelText: "City",
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: AppColors.accentColor(),
+                                      strokeAlign:BorderSide.strokeAlignOutside
+                                  )
+                              )
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        TextField(
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.pin_drop),
+                              labelText: "Pincode",
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: AppColors.accentColor(),
+                                      strokeAlign:BorderSide.strokeAlignOutside
+                                  )
+                              )
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        TextField(
+                          keyboardType: TextInputType.text,
+                          decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.keyboard),
+                              labelText: "Remarks",
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: AppColors.accentColor(),
+                                      strokeAlign:BorderSide.strokeAlignOutside
+                                  )
+                              )
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text("Payment Method",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+
+                        ),),
+                        Column(
+                          children: <Widget>[
+                            RadioListTile<PaymentMethod>(
+                              title: Text('Cash on delivery'),
+                              value: PaymentMethod.cashOnDelivery,
+                              groupValue: selectedPaymentMethod,
+                              onChanged: (PaymentMethod? value) {
+                                setState(() {
+                                  selectedPaymentMethod = value!;
+                                });
+                              },
+                            ),
+                            RadioListTile<PaymentMethod>(
+                              title: const Text('Online'),
+                              value: PaymentMethod.online,
+                              groupValue: selectedPaymentMethod,
+                              onChanged: (PaymentMethod? value) {
+                                setState(() {
+                                  selectedPaymentMethod = value!;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          child: MaterialButton(onPressed: (){
+
+                          },
+                            color: AppColors.accentColor(),
+                            textColor: AppColors.textColor(),
+                            child: Text("Place Order"),
+                          ),
+                        )
                       ],
                     ),
                 ),
